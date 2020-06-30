446757b1ad1091ccc35d4bb241da7924ebd01dbd
neringabickmore <63475828+neringabickmore@users.noreply.github.com>
2020-06-30 11:15:11 +0100

    Defined coinImages & coinDisplay for each level

--- assets/js/main.js
+++ assets/js/main.js
@@ -340,11 +340,37 @@ function unMuteAudio() {
 // Game Dashboard:
 // 1.3 Define highlighted star display for each completed task
 //
+const displayCoinsL1 = [1, 2, 5, 10];
+const displayCoinsL2 = [1, 2, 5, 10, 20];
+const displayCoinsL3 = [1, 2, 5, 10, 20, 50];
+const coinImagesL1 = [
+  "assets/images/1p.png",
+  "assets/images/2p.png",
+  "assets/images/5p.png",
+  "assets/images/10p.png",
+];
+const coinImagesL2 = [
+  "assets/images/1p.png",
+  "assets/images/2p.png",
+  "assets/images/5p.png",
+  "assets/images/10p.png",
+  "assets/images/20p.png",
+];
+const coinImagesL3 = [
+  "assets/images/1p.png",
+  "assets/images/2p.png",
+  "assets/images/5p.png",
+  "assets/images/10p.png",
+  "assets/images/20p.png",
+  "assets/images/50p.png",
+];
 
 let currentLevel = 1,
   currentTask = 1,
   gameStageDisplay = {
     "Level 1": {
+      displayCoinsL1,
+      coinImagesL1,
       "Task 1": {},
       "Task 2": {},
       "Task 3": {},
@@ -353,6 +379,8 @@ let currentLevel = 1,
       "Task 6": {},
     },
     "Level 2": {
+      displayCoinsL2,
+      coinImagesL2,
       "Task 1": {},
       "Task 2": {},
       "Task 3": {},
@@ -360,7 +388,9 @@ let currentLevel = 1,
       "Task 5": {},
       "Task 6": {},
     },
-    "Level 1": {
+    "Level 3": {
+      displayCoinsL3,
+      coinImagesL3,
       "Task 1": {},
       "Task 2": {},
       "Task 3": {},
@@ -380,6 +410,5 @@ function gameStage() {
 
 // Game Console:
 // 2.1 Define price tag display in each level
-// 2.2 Define coin display in each level
 // 2.3 Definte Initial sum  variable
 // 2.4 Write a function for the sum variable
