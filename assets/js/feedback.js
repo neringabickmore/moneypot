c1d67d653c568f69a202620ca595bf0ac5af25e1
neringabickmore <63475828+neringabickmore@users.noreply.github.com>
2020-07-25 21:39:52 +0100

    Deleted dead files

--- assets/js/feedback.js
+++
@@ -1,9 +0,0 @@
-/**
- * code for fetching name info to feedback.html
- * Original code from: https: //github.com/neringabickmore/BI-Team-5/blob/master/assets/js/script.js
- */
-
-let url_string = window.location.href;
-let url = new URL(url_string);
-let yourName = url.searchParams.get("name");
-document.getElementById("yourName").innerHTML = yourName;
